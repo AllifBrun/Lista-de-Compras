6c34a55e476771da2798b2e561ff3911d1d7cbe7
allif <allifbrun@gmail.com>
2024-09-17 21:27:24 -0300

    configurando o botao de deletar

--- script/app.js
+++ script/app.js
@@ -1,45 +1,62 @@
 const input = document.querySelector("input");
 const form = document.querySelector("form");
 
-form.onsubmit = (e)=>{
+form.onsubmit = (e) => {
 
-e.preventDefault();
+    e.preventDefault();
 
-if(input.value == ''){
-    alert("Digite algo!")
-}
-else{
-
-
-    console.log("foi")
-
-    const itens = document.querySelector(".itens")
-
-    const div = document.createElement('div')
-    div.className = 'item';
-
-    const label = document.createElement('label')
-    label.className = 'checado';
-
-    const novoInput = document.createElement('input')
-    novoInput.setAttribute('type', 'checkbox')
-    novoInput.className = 'check'
-
-    const p = document.createElement('p')
-    p.innerText = input.value;
-
-    const img = document.createElement('img')
-    img.className = 'deletar'
-    img.setAttribute('src', './assets/icons/delete-02-stroke-rounded.svg')
+    if (input.value == '') {
+        alert("Digite algo!")
+    }
+    else {
 
 
+        console.log("foi")
+
+        const itens = document.querySelector(".itens")
+
+        const div = document.createElement('div')
+        div.className = 'item';
+
+        const label = document.createElement('label')
+        label.className = 'checado';
+
+        const novoInput = document.createElement('input')
+        novoInput.setAttribute('type', 'checkbox')
+        novoInput.className = 'check'
+
+        const p = document.createElement('p')
+        p.innerText = input.value;
+
+        const img = document.createElement('img')
+        img.className = 'deletar'
+        img.setAttribute('src', './assets/icons/delete-02-stroke-rounded.svg')
 
 
-    itens.appendChild(div);
-    div.appendChild(label)
-    label.appendChild(novoInput);
 
-    div.appendChild(p)
-    div.appendChild(img)
-}
+
+        itens.appendChild(div);
+        div.appendChild(label)
+        label.appendChild(novoInput);
+
+        div.appendChild(p)
+        div.appendChild(img)
+
+        input.value = ""
+
+
+        const deletar = document.querySelectorAll('.deletar');
+        deletar.forEach((btn) => {
+            btn.onclick = (e) => {
+                e.preventDefault();
+                const el = e.target; 
+                if (el.classList.contains('deletar')) {
+                    const itemPai = el.closest('.item'); 
+                    itemPai.remove(); 
+                    console.log('Item deletado!');
+                }
+            };
+        });
+
+    }
 }
